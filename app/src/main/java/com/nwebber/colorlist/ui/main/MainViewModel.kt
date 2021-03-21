1c8688e0bef635c63d4aee72012458b7ff0bdaa0
NateWebber <nwebber@u.rochester.edu>
2021-03-21 15:57:22 -0400

    MainViewModel.kt finished(?), added data.json

--- app/src/main/java/com/nwebber/colorlist/ui/main/MainViewModel.kt
+++ app/src/main/java/com/nwebber/colorlist/ui/main/MainViewModel.kt
@@ -1,7 +1,33 @@
 package com.nwebber.colorlist.ui.main
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.google.gson.Gson
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class ColorVocab : ArrayList<ColorDefinition>()
+
+data class ColorDefinition(val id: Int, val hexString: String, val rgb: Triple<Int, Int, Int>, val hsl: Triple<Int, Int, Int>, val name: String)
+
+class ColorList(jsonString: String) {
+    var colors = ArrayList<ColorDefinition>()
+    init {
+        val gson = Gson()
+        colors = gson.fromJson(jsonString, ColorVocab::class.java)
+    }
+}
+
+class MainViewModel(app: Application) : AndroidViewModel(app) {
+    private lateinit var colorList: ColorList
+
+    private val _colorVocab = MutableLiveData<List<ColorDefinition>>()
+    var colorVocab: LiveData<List<ColorDefinition>> = _colorVocab
+
+    init {
+        val jsonString = app.assets.open("data.json").bufferedReader().use {it.readText()}
+        colorList = ColorList(jsonString)
+
+        _colorVocab.value = colorList.colors
+    }
 }
