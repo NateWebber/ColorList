bc0bb6d464c99ff2ec9b3b6b3e62e012c400ea1b
NateWebber <nwebber@u.rochester.edu>
2021-03-21 16:36:34 -0400

    app runs, need to fix text view and colors

--- app/src/main/java/com/nwebber/colorlist/ui/main/MainFragment.kt
+++ app/src/main/java/com/nwebber/colorlist/ui/main/MainFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.lifecycle.observe
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.nwebber.colorlist.R
